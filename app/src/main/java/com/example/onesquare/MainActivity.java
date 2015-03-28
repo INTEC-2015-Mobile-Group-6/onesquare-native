cba2da34b0bbb802c64766686380e7b4d87f8cd0
Francis Brito <fr.br94@gmail.com>
2015-03-28 13:12:09 -0400

    Add tabs to main activity.

--- app/src/main/java/com/example/onesquare/MainActivity.java
+++ app/src/main/java/com/example/onesquare/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.onesquare;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TabHost;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,7 +19,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new CheckInListFragment())
                     .commit();
         }
     }
@@ -51,16 +50,43 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class CheckInListFragment extends Fragment {
 
-        public PlaceholderFragment() {
+        private static final String TAB_ALL_CHECK_INS = "all_check_ins_tab";
+        private static final String TAB_FAV_CHECK_INS = "fav_check_ins_tab";
+
+        public CheckInListFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            setUpTabs(rootView);
+
             return rootView;
+        }
+
+        private void setUpTabs(View rootView) {
+            TabHost tabHost = (TabHost) rootView.findViewById(R.id.tab_host);
+            tabHost.setup();
+
+            TabHost.TabSpec allCheckInsTabSpec = tabHost.newTabSpec(TAB_ALL_CHECK_INS);
+            TabHost.TabSpec favoriteCheckInsTabSpec = tabHost.newTabSpec(TAB_FAV_CHECK_INS);
+
+            String allCheckInsLabel = getResources()
+                    .getString(R.string.tab_all_check_ins);
+            allCheckInsTabSpec.setIndicator(allCheckInsLabel);
+            allCheckInsTabSpec.setContent(R.id.all_check_ins_tab);
+
+            String favoriteCheckInsLabel = getResources()
+                    .getString(R.string.tab_fav_check_ins);
+            favoriteCheckInsTabSpec.setIndicator(favoriteCheckInsLabel);
+            favoriteCheckInsTabSpec.setContent(R.id.fav_check_ins_tab);
+
+            tabHost.addTab(allCheckInsTabSpec);
+            tabHost.addTab(favoriteCheckInsTabSpec);
         }
     }
 }
